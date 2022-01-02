416cacdd29144f2849921a63921afd5e00709c9a
Kazuki Ueki <ueki.kazuki@classmethod.jp>
2022-01-02 12:39:42 +0900

    Use net.pipe for unittesting

--- chaser/chaser_test.go
+++ chaser/chaser_test.go
@@ -1,6 +1,8 @@
 package chaser
 
 import (
+	"net"
+	"net/textproto"
 	"reflect"
 	"testing"
 )
@@ -14,7 +16,7 @@ func TestClient_GetReady(t *testing.T) {
 	tests := []struct {
 		name   string
 		fields fields
-		want   []int8
+		want   []int
 	}{
 		{
 			name: "Test",
@@ -23,19 +25,33 @@ func TestClient_GetReady(t *testing.T) {
 				host: "127.0.0.1",
 				port: 2009,
 			},
-			want: []int8{0, 0, 0, 0, 0, 0, 0, 0, 0},
+			want: []int{0, 0, 0, 0, 0, 0, 0, 0, 0},
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			client, err := NewClient(
-				tt.fields.name,
-				tt.fields.host,
-				tt.fields.port)
+			s, c := net.Pipe()
+			client := &Client{
+				conn: textproto.NewConn(c),
+			}
+			defer client.Close()
+			go func() {
+				conn := textproto.NewConn(s)
+				conn.PrintfLine("%s", "@")
+
+				res, err := conn.ReadLine()
 			if err != nil {
 				t.Error(err)
-				return
 			}
+				switch res {
+				case "gr":
+					conn.PrintfLine("%v", "1000000000")
+				default:
+					t.Logf("%v", res)
+				}
+				conn.Close()
+			}()
+
 			got, err := client.GetReady()
 			if err != nil {
 				t.Error(err)
